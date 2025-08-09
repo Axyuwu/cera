89e6e173a4a6bce7b4e6c4a5ae79dfaca741f33d
Axy <gilliardmarthey.axel@gmail.com>
2025-08-09 21:28:44 +0200

    Added aggr_slice_concat to the builtins

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn main() {
     let builtins_str = std::fs::read_to_string("./builtins.cera").unwrap();
     let builtins = parse::Atom::parse_module(&builtins_str).unwrap();
     let builtin_lookup = Arc::new(BuiltinLookup::new(atom_to_val_raw(builtins)));
-    let input = std::fs::read_to_string("./examples/hello_world_interpreted.cera").unwrap();
+    let input = std::fs::read_to_string("./builtins_builder.cera").unwrap();
     let module = parse::Atom::parse_module(&input).unwrap();
     let start = Instant::now();
     let res = builtins::eval_builtin(module, &builtin_lookup);
