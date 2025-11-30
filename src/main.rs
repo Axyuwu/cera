8a5c7e39725ab4999bfc66d56a33db8c1b24d2f5
Axy <gilliardmarthey.axel@gmail.com>
2025-11-30 17:27:58 +0100

    Clean slate

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,7 @@
-use std::time::Instant;
-
-use crate::{
-    builtins::{atom_to_val_raw, builtin_values::BuiltinLookup},
-    utils::sync::cache_arc::CacheArc as Arc,
-    write::write_module_pretty,
-};
-
-mod builtins;
 mod parse;
-pub mod utils;
-mod write;
 
 fn main() {
-    let builtins_str = std::fs::read_to_string("./builtins.cera").unwrap();
-    let builtins = parse::Atom::parse_module(&builtins_str).unwrap();
-    let builtin_lookup = Arc::new(BuiltinLookup::new(atom_to_val_raw(builtins)));
-    let input = std::fs::read_to_string("./builtins_builder.cera").unwrap();
-    let module = parse::Atom::parse_module(&input).unwrap();
-    let start = Instant::now();
-    let res = builtins::eval_builtin(module, &builtin_lookup);
-    let taken = start.elapsed().as_secs_f32();
-    print!("{}", write_module_pretty(&res));
-    eprintln!("taken: {}", taken);
+    let test_str = std::fs::read_to_string("./test.cera").unwrap();
+    let test = parse::Atom::parse_module(&test_str).unwrap();
+    dbg!(test);
 }
--- src/utils/sync.rs
+++
@@ -1,4 +0,0 @@
-pub mod cache_arc;
-pub mod cache_lock;
-pub mod spinmutex;
-pub mod sync_map;
