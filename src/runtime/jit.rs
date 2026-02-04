6b53e81bb1edeb4c02344cff9dc4a1a882171f1a
Axy <gilliardmarthey.axel@gmail.com>
2026-02-04 02:38:51 +0100

    simple jit graph start

--- src/runtime/jit.rs
+++ src/runtime/jit.rs
@@ -1,68 +1,134 @@
-enum Type {}
+use std::{
+    marker::PhantomData,
+    ops::{Index, IndexMut},
+};
 
-enum ErrorHandling {
-    HostUB,
-    ProgramUB,
-    ArithSaturating,
-    ArithOverflowing,
-    DivideToZero,
+use derive_more::From;
+
+struct ID<T> {
+    idx: usize,
+    _phantom: PhantomData<T>,
 }
 
-enum Constant {}
-
-enum Expression {
-    Mul {},
+impl<T> ID<T> {
+    fn new(idx: usize) -> Self {
+        ID {
+            idx,
+            _phantom: PhantomData,
+        }
+    }
 }
 
-enum BlockID {
-    Entry,
-    Branch(usize),
-    Exit,
+impl<T> Clone for ID<T> {
+    fn clone(&self) -> Self {
+        Self {
+            idx: self.idx.clone(),
+            _phantom: self._phantom.clone(),
+        }
+    }
 }
 
-enum ValueID {
-    Constant(usize),
-    Expression(usize),
-    Pred(usize),
+impl<T> Copy for ID<T> {}
+
+struct Datastore<T> {
+    store: Vec<T>,
 }
 
-struct EntryBlock {
-    args: Vec<Type>,
-    succ: BlockID,
+impl<T> Default for Datastore<T> {
+    fn default() -> Self {
+        Self {
+            store: Default::default(),
+        }
+    }
 }
 
-struct PredBlock {
-    block_ids: Vec<BlockID>,
-    value_ids_flat: Vec<ValueID>,
+impl<T> Index<ID<T>> for Datastore<T> {
+    type Output = T;
+
+    fn index(&self, index: ID<T>) -> &Self::Output {
+        &self.store[index.idx]
+    }
 }
 
-enum SuccBlock {
-    Unconditional(BlockID),
-    Conditional {
-        condition: ValueID,
-        table: Vec<BlockID>,
-    },
+impl<T> IndexMut<ID<T>> for Datastore<T> {
+    fn index_mut(&mut self, index: ID<T>) -> &mut Self::Output {
+        &mut self.store[index.idx]
+    }
 }
 
-struct BranchBlock {
-    pred: PredBlock,
-    expressions: Vec<Expression>,
-    succ: SuccBlock,
+impl<T> Datastore<T> {
+    fn insert(&mut self, val: T) -> ID<T> {
+        let res = self.next_id();
+        self.store.push(val);
+        res
+    }
+
+    fn next_id(&self) -> ID<T> {
+        ID::new(self.store.len())
+    }
+
+    fn remove(&mut self, id: ID<T>) -> (T, Option<Rename<ID<T>>>) {
+        if id.idx == self.store.len() - 1 {
+            return (self.store.pop().unwrap(), None);
+        }
+        let from = ID::new(self.store.len() - 1);
+        let to = id;
+        let moved = self.store.pop().unwrap();
+        (
+            std::mem::replace(&mut self.store[id.idx], moved),
+            Some(Rename { to, from }),
+        )
+    }
 }
 
-struct ExitBlock {
-    res: PredBlock,
+struct Rename<T> {
+    from: T,
+    to: T,
 }
 
-struct Function {
-    entry: EntryBlock,
-    blocks: Vec<BranchBlock>,
-    exit: ExitBlock,
+#[derive(From)]
+struct Pred(EdgeID);
+type PredID = ID<Pred>;
+
+#[derive(From)]
+struct Succ(EdgeID);
+type SuccID = ID<Succ>;
+
+#[derive(Default)]
+struct Node {
+    pred: Datastore<Pred>,
+    succ: Datastore<Succ>,
+}
+type NodeID = ID<Node>;
+
+struct Edge {
+    src: (NodeID, SuccID),
+    dst: (NodeID, PredID),
+}
+type EdgeID = ID<Edge>;
+
+#[derive(Default)]
+struct Graph {
+    nodes: Datastore<Node>,
+    edges: Datastore<Edge>,
 }
 
-struct CodegenUnit {
-    constants: Vec<Constant>,
-    functions: Vec<Function>,
+impl Graph {
+    fn add_node(&mut self) -> NodeID {
+        self.nodes.insert(Default::default())
+    }
+    fn add_edge(&mut self, src: NodeID, dst: NodeID) -> EdgeID {
+        let edge_id = self.edges.next_id();
+        let src_succ = self.nodes[src].succ.insert(edge_id.into());
+        let dst_pred = self.nodes[dst].pred.insert(edge_id.into());
+        let edge = Edge {
+            src: (src, src_succ),
+            dst: (dst, dst_pred),
+        };
+        self.edges.insert(edge)
+    }
 }
+
+struct CodegenUnit {}
 
 struct LinkableBlob {}
